951fe93b341fb25ef227c3071322974a90e90ec4
MoussaJMC <59544939+MoussaJMC@users.noreply.github.com>
2025-05-30 01:10:17 +0000

    Update dependencies and start development server

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
-import './utils/i18n.ts';
+import './utils/i18n';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+createRoot(rootElement).render(
   <StrictMode>
     <AuthProvider>
       <App />
