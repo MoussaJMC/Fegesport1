5ba4ff2e6e775e1f9fc6081486113f092eaf9939
MoussaJMC <59544939+MoussaJMC@users.noreply.github.com>
2025-06-01 13:31:43 +0200

    Fix StackBlitz preview issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: true
+    host: '0.0.0.0',
+    hmr: {
+      clientPort: 443
+    }
   },
   optimizeDeps: {
     exclude: [
